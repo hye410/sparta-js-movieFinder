dc80bc44f60d9e365c537d27cd32e2535b60c4b9
이다혜 <dahye@DHs-MacBook-Air.local>
2025-01-09 16:26:49 +0900

    [개인과제] api 가져와서 메인페이지 레이아웃 그리기 완성

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTEyNzBkMDYxZTliNzQ0NmZiNzBiOGYzNWExNjlkYyIsIm5iZiI6MTczNjI5NTgyNi45NjgsInN1YiI6IjY3N2RjNTkyYjExZDA4ODExMTdhZjczNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kECnKC_ZIyWp-vTFYGO9m4QSto2APLO3axKckaOs11Q`
-  }
+import { MovieCard } from "./MovieCard.js";
+import { API_KEY } from "./private.js";
+
+////// Dom 조작 변수 /////
+const $main = document.getElementById('main');
+/////////// //////////
+
+getMovieData(); // 서버에서 data를 불러온다.
+
+let movieData = new Array();
+
+ async function getMovieData () {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+    }
+  };
+  
+  await fetch('https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1', options)
+    .then(res => res.json())
+    .then(res => {
+      const { results } = res;
+      movieData = parseData(results);
+      makeMovieCard(movieData);
+    })
+    .catch(err => {
+      alert(err?.status_message || '에러가 발생했습니다.');
+    });
 };
 
-fetch('https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1', options)
-  .then(res => res.json())
-  .then(res => console.log('!!!!!!res',res))
-  .catch(err => console.error(err));
+const parseData = (data) => {
+  if (data.length === 0) return;
+  return data.map((_data) => {
+    const { id, title, vote_average, poster_path } = _data;
+    return { id, title, rate : vote_average, img : poster_path } 
+  })
+}
+
+const makeMovieCard = (movieData) => {
+  movieData.forEach((data) => {
+    $main.appendChild(MovieCard(data));
+  })
+}
+
+
+
+
+
 
